b674b59e259c4456aaed47c6370765c80db0f3ed
AleksandrVassiljuk <Vassiljukaleksandr@gmail.com>
2026-06-02 13:30:36 +0300

    Update api.ts

--- Movie-app/src/services/api.ts
+++ Movie-app/src/services/api.ts
@@ -1,28 +1,26 @@
 import axios from "axios";
 import type { Movie } from "../types/Movie";
 
-const API_TOKEN = import.meta.env.VITE_API_TOKEN as string;
+const API_KEY = import.meta.env.VITE_API_KEY as string;
 
 const api = axios.create({
   baseURL: "https://api.themoviedb.org/3",
-  headers: {
-    Authorization: `Bearer ${API_TOKEN}`,
-    Accept: "application/json",
-  },
 });
 
 export const getPopularMovies = () => {
-  return api.get<{ results: Movie[] }>("/movie/popular");
+  return api.get<{ results: Movie[] }>(
+    `/movie/popular?api_key=${API_KEY}`
+  );
 };
 
 export const searchMovies = (query: string) => {
   return api.get<{ results: Movie[] }>(
-    `/search/movie?query=${encodeURIComponent(query)}`
+    `/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`
   );
 };
 
 export const getMovieDetails = (id: string | number) => {
-  return api.get<Movie>(`/movie/${id}`);
+  return api.get<Movie>(
+    `/movie/${id}?api_key=${API_KEY}`
+  );
 };
-
-export default api;
